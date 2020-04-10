09e1b1a59f2369b1fcf637c33d9b0520271068a0
janakievski-blagoj <janakievskibaze@gmail.com>
2020-04-10 21:29:41 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -15,15 +15,52 @@ class Student {
 	}};
 
 	//TODO constructor
+	public Student(String index, String firstName, String lastName) {
+                this.index = index;
+                this.firstName = firstName;
+                this.lastName = lastName;
+        }
 
 	//TODO seters & getters
+    public String getIndex() {
+                return this.index;
+        }
+    public String getFirstName() {
+                return this.firstName;
+        }
+    public String getLastName() {
+                return this.lastName;
+        }
+
+    public void setIndex(String index) {
+                this.index = index;
+        }
+    public void setFirstName(String firstName) {
+                this.firstName = firstName;
+        }
+    public void setLastName(String lastName) {
+                this.lastName = lastName;
+        }
+
+
+
 
 	public double getAverage() {
 		//TODO
+        float average;
+        for(int i=0; i<labPoints.length; i++)
+            average += labPoints[i];
+        average = average * 1.0 / labPoints.length;
+        return average;
+
 	}
 
 	public boolean hasSignature() {
 		//TODO
+        if(labPoints.length > 8)
+            return true
+        else return false
+
 	}
 }
 
